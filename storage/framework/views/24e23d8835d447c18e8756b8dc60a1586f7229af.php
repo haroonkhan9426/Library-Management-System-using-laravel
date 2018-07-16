87ff4b3826c0bbe148ca9ec7d274a44254c1950a
haroonkhan9426 <Haroonkhan9426@gmail.com>
2018-07-16 11:01:15 +0500

    Added authentication and logout

---
+++ storage/framework/views/24e23d8835d447c18e8756b8dc60a1586f7229af.php
@@ -0,0 +1,50 @@
+<?php $__env->startSection('content'); ?>
+<div class="row">
+  <form class="" action="/issueBooks" method="post">
+    <?php echo e(csrf_field()); ?>
+
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">First Portion</div>
+      <div class="panel-body">
+        <div class="form-group">
+          <label for="memberId">Member ID</label>
+          <input type="text" class="form-control"name="memberId" value="" id="bookName" placeholder="Member ID">
+        </div>
+        <div class="form-group">
+          <label for="bookName">Book Name</label>
+          <input type="text" class="form-control"name="bookName" value="" id="bookName" placeholder="Member ID">
+        </div>
+        <div class="form-group">
+          <label for="bookId">Book ID</label>
+          <input type="text" class="form-control"name="bookId" value="" id="bookId" placeholder="Book ID">
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">Second Portion</div>
+      <div class="panel-body">
+        <div class="form-group">
+          <label for="issueDate">Issue Date</label>
+        <input type="date" class="form-control" name="issueDate" id="issueDate">
+        </div>
+        <div class="form-group">
+          <label for="returnDate">Return Date</label>
+        <input type="date" name="returnDate" class="form-control" id="returnDate">
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary" name="button">Issue Books</button>
+        </div>
+    </div>
+  </div>
+  </form>
+
+</div>
+
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
