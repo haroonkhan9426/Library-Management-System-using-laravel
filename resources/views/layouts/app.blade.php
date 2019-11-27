cc57fc83274c9a651449d5bce1373f885f26945a
YoshinoTakigawa <anirban.internships.training@gmail.com>
2019-11-27 20:06:02 +0530

    new changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'MCD Library') }}</title>
+    <title>{{ config('app.name', 'DCSE Library') }}</title>
 
     <!-- Styles -->
 
